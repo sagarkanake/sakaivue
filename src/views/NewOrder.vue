76be6cb366104feaf7ffca605f0ef07679cfbabb
Sankalpa <sankalpab@pharmarack.com>
2024-07-22 11:21:31 +0530

    New order css fixes

--- src/views/NewOrder.vue
+++ src/views/NewOrder.vue
@@ -1,21 +1,35 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onBeforeMount } from 'vue';
+import { FilterMatchMode } from 'primevue/api';
+import { ProductService } from '@/service/ProductService';
 
 const dropdownItems = ref([
     { name: 'Option 1', code: 'Option 1' },
     { name: 'Option 2', code: 'Option 2' },
     { name: 'Option 3', code: 'Option 3' }
 ]);
-
+const filters = ref({});
 const dropdownItem = ref(null);
+onBeforeMount(() => {
+    initFilters();
+});
+onMounted(() => {
+    productService.getProducts().then((data) => (products.value = data));
+});
+
+const initFilters = () => {
+    filters.value = {
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS }
+    };
+};
 </script>
 
 <template>
     <div class="grid" :style="{ 'margin-left': '-39px', 'margin-top': '-14px','margin-right': '-40px' }">
         <div :style="{ width: '64%','margin-top':'14px' }">
             <div class="card">
-                <h5>Order Details</h5>
-                <div>
+                <h5 :style="{'color':'gray','font-size': 'medium','margin-left': '-10px'}">Order Details</h5>
+                <div :style="{'margin-left':'-22px'}">
                     <!-- First Row -->
                     <div class="form-row flex flex-wrap">
                         <div class="field col-12 md:col-4">
@@ -33,21 +47,21 @@ const dropdownItem = ref(null);
                     </div>
 
                     <!-- Second Row -->
-                    <div class="flex gap-2">
-                        <div class="field" :style="{'margin-top':'-15px','margin-left':'10px'}">
+                    <div class="flex gap-2" :style="{'margin-top':'2px','margin-left':'15px'}">
+                        <div class="field">
                             <label for="deliveryMethod" :style="{'font-weight':'bold'}">Delivery Location</label>
-                            <Dropdown id="deliveryMethod" v-model="selectedDeliveryMethod" :options="deliveryMethods"  placeholder="Select"  />
+                            <Dropdown id="deliveryMethod" v-model="selectedDeliveryMethod" :options="deliveryMethods"  class="custom-dropdown" placeholder="Select" />
                             <!-- <Dropdown id="deliveryLocation" v-model="selectedDeliveryLocation" :options="deliveryLocations" placeholder="Select Delivery Location" /> -->
                         </div>
-                        <div class="field" :style="{'margin-top':'-15px','margin-left':'10px'}">
+                        <div class="field flex-1">
                             <label for="deliveryMethod" :style="{'font-weight':'bold'}">Delivery Method</label>
                             <Dropdown id="deliveryMethod" v-model="selectedDeliveryMethod" :options="deliveryMethods" placeholder="Select" :style="{'font-size':'11px'}" />
                         </div>
-                        <div  class="field" :style="{'margin-top':'-15px','margin-left':'10px'}">
+                        <div  class="field">
                             <label for="deliveryWindow" :style="{'font-weight':'bold'}">Delivery Window</label>
                             <Dropdown id="deliveryWindow" v-model="selectedDeliveryWindow" :options="deliveryWindows" placeholder="Select" :style="{'font-size':'11px'}" />
                         </div>
-                        <div class="field" :style="{'margin-top':'-15px','margin-left':'10px'}">
+                        <div class="field">
                             <label for="deliveryDate" :style="{'font-weight':'bold'}">Delivery Date</label>
                             <Calendar id="deliveryDate" v-model="deliveryDate" showIcon placeholder = "Select" :style="{'font-size':'11px'}" />
                         </div>
@@ -65,8 +79,7 @@ const dropdownItem = ref(null);
                 </div>
             </div>
         </div>
-
-        <div :style="{ width: '35%' }" class="md:col-6">
+        <div :style="{ width: '36%' }" class="md:col-6">
             <div class="card p-fluid" :style="{}">
                 <h5>Payment</h5>
                 <div :style="{ display: 'flex', 'flex-direction': 'column', gap: '16px' }">
@@ -164,5 +177,117 @@ const dropdownItem = ref(null);
                 </div>
             </div>
         </div>
+<div class="card" :style="{ width: '64%','margin-top': '-52%'}">
+     <DataTable
+                    ref="dt"
+                    :value="products"
+                    v-model:selection="selectedProducts"
+                    dataKey="id"
+                    :paginator="true"
+                    :rows="10"
+                    :filters="filters"
+                    paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                    :rowsPerPageOptions="[5, 10, 25]"
+                    currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+                >
+                    <template #header>
+                        <div class="flex flex-column md:flex-row md:justify-content-between md:align-items-center" :style="{ 'margin-top': '5px'}">
+                            <h5 style="color:#808080">Line Items</h5>
+                        </div>
+                        <div class="flex justify-content-between gap-6 mt-2">
+                            <div class="flex gap-2">
+                                <div>
+                                    <IconField iconPosition="left" class="block mt-2 md:mt-0">
+                                        <InputIcon class="pi pi-search" />
+                                        <InputText class="w-full sm:w-auto" v-model="filters['global'].value" placeholder="Search..." />
+                                    </IconField>
+                                </div>
+                                
+                            </div>
+                            <div class="flex gap-2">
+                                <Button type="button" label="Remove" icon="pi pi-trash" :style="{ 'background-color': '#DFEDDF', border: '#DFEDDF' }" @click="exportCSV($event)"></Button>
+                                <Button type="button" label="Add" icon="pi pi-plus" :style="{ 'background-color': '#1E4A35', border: '#1E4A35' }" @click="newOrder()"></Button>
+                            </div>
+                        </div>
+                    </template>
+                    <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+
+                    <Column field="productId" header="Product ID" :sortable="true" headerStyle="width:14%; min-width:10rem;">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Order No.</span>
+                            {{ slotProps.data.code }}
+                        </template>
+                    </Column>
+                    <Column field="item" header="Item" :sortable="true" headerStyle="width:14%; min-width:10rem;">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Customer</span>
+                            {{ slotProps.data.name }}
+                        </template>
+                    </Column>
+                    <Column field="grade" header="Grade" headerStyle="width:14%; min-width:10rem;">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Order Date</span>
+                            <img :src="'/demo/images/product/' + slotProps.data.image" :alt="slotProps.data.image" class="shadow-2" width="100" />
+                        </template>
+                    </Column>
+                    <Column field="unit" header="Unit" :sortable="true" headerStyle="width:14%; min-width:10rem;">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Status</span>
+                            <Tag :severity="getBadgeSeverity(slotProps.data.inventoryStatus)">{{ slotProps.data.inventoryStatus }}</Tag>
+                        </template>
+                    </Column>
+                    <Column field="stdWgt" header="Std Wgt(G)" :sortable="true" headerStyle="width:14%; min-width:8rem;">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Items</span>
+                            {{ formatCurrency(slotProps.data.price) }}
+                        </template>
+                    </Column>
+                    <Column field="invEq" header="INV eq(KG)" :sortable="true" headerStyle="width:14%; min-width:10rem;">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Amount</span>
+                            {{ slotProps.data.category }}
+                        </template>
+                    </Column>
+                    <Column field="processing" header="Processing" :sortable="true" headerStyle="width:14%; min-width:10rem;">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Delivery</span>
+                            <Rating :modelValue="slotProps.data.rating" :readonly="true" :cancel="false" />
+                        </template>
+                    </Column>
+                    <Column field="specificatios" header="specifications" :sortable="true" headerStyle="width:14%; min-width:10rem;">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Delivery Date</span>
+                            <Rating :modelValue="slotProps.data.rating" :readonly="true" :cancel="false" />
+                        </template>
+                    </Column>
+                    <Column field="unitCost" header="Unit Cost" :sortable="true" headerStyle="width:14%; min-width:10rem;">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Delivery Window</span>
+                            <Rating :modelValue="slotProps.data.rating" :readonly="true" :cancel="false" />
+                        </template>
+                    </Column>
+                    <Column field="payment" header="Payment" :sortable="true" headerStyle="width:14%; min-width:10rem;">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Payment</span>
+                            <Rating :modelValue="slotProps.data.rating" :readonly="true" :cancel="false" />
+                        </template>
+                    </Column>
+                    <Column headerStyle="min-width:10rem;">
+                        <template #body>
+                            <Button icon="pi pi-ellipsis-v" type="button" class="p-button-text"></Button>
+                        </template>
+                    </Column>
+                </DataTable>
+   
+</div>
+        
     </div>
 </template>
+<style scoped>
+/* Scoped styles to target the placeholder text in the dropdown */
+.custom-dropdown .p-dropdown-label.p-placeholder {
+
+  font-size: 12px !important; /* Adjust the font size as needed */
+  color: #e21818 !important; /* Adjust the color if needed */
+}
+</style>
